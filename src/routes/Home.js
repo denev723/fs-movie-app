dd8e1492a96da6f37c45ce932983091be150ff6c
denev <denev723@gmail.com>
2025-01-01 23:42:59 +0900

    initialize, home router, headline, search component

---
+++ src/routes/Home.js
@@ -0,0 +1,12 @@
+import { Component } from "../core/denev";
+import Headline from "../components/Headline";
+import Search from "../components/Search";
+
+export default class Home extends Component {
+  render() {
+    const headline = new Headline().el;
+    const search = new Search().el;
+    this.el.classList.add("container");
+    this.el.append(headline, search);
+  }
+}
