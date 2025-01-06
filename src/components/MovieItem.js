edbb9a26872752209ac4e8c8c00252a8ab6563b7
denev <denev723@gmail.com>
2025-01-06 23:30:16 +0900

    movielist, movieitem, viewmore, loading el

---
+++ src/components/MovieItem.js
@@ -0,0 +1,29 @@
+import { Component } from "../core/denev";
+
+export default class MovieItem extends Component {
+  constructor(props) {
+    super({
+      props,
+      tagName: "a",
+    });
+  }
+  render() {
+    const {
+      movie: { Poster, Title, Year, imdbId },
+    } = this.props;
+
+    this.el.setAttribute("href", `#/movie?id=${imdbId}`);
+    this.el.classList.add("movie");
+    this.el.style.backgroundImage = `url(${Poster})`;
+    this.el.innerHTML = /* html */ `
+        <div class="info">
+            <div class="year">
+                ${Year}
+            </div>
+            <div class="title">
+                ${Title}
+            </div>
+        </div>
+    `;
+  }
+}
