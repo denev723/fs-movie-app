75fbe227890b91bdd54d8b5b86a55b2af2ebc864
denev <denev723@gmail.com>
2025-01-13 09:28:05 +0900

    finish coding

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/denev";
+
+export default new Store({
+  photo: "https://heropy.blog/css/images/logo.png",
+  name: "Denev / Kim Byeongjoo",
+  email: "denev723@gmail.com",
+  blog: "https://blog.naver.com/denev723",
+  github: "https://github.com/denev723",
+  repository: "https://github.com/denev723/fs-movie-app",
+});
