5de24cdc8f350b80cdaff9f96b13a75b47bf6704
denev <denev723@gmail.com>
2025-01-14 21:47:33 +0900

    오류 수정

--- api/movieHandler.js
+++ api/movieHandler.js
@@ -6,8 +6,8 @@ const { OMDB_API_KEY } = process.env;
 export default async function hanlder(request, response) {
   const { title, page, id } = JSON.parse(request.body);
   const url = id
-    ? `https://www.omdbapi.com?apikey=${OMDB_API_KEY}c&i=${id}&plot=full`
-    : `https://www.omdbapi.com?apikey=${OMDB_API_KEY}c&s=${title}&page=${page}`;
+    ? `https://www.omdbapi.com?apikey=${OMDB_API_KEY}&i=${id}&plot=full`
+    : `https://www.omdbapi.com?apikey=${OMDB_API_KEY}&s=${title}&page=${page}`;
   const res = await fetch(url);
   const json = await res.json();
   response.status(200).json(json);
