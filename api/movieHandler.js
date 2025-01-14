42577e5957c39da486a4a99e5b6daa7a4aee723c
denev <denev723@gmail.com>
2025-01-14 21:40:36 +0900

    보안 강화

--- api/movieHandler.js
+++ api/movieHandler.js
@@ -6,8 +6,8 @@ const APIKEY = process.env.OMDB_AP_KEY;
 export default async function hanlder(request, response) {
   const { title, page, id } = JSON.parse(request.body);
   const url = id
-    ? `https://omdbapi.com?apikey=${APIKEY}c&i=${id}&plot=full`
-    : `https://omdbapi.com?apikey=${APIKEY}c&s=${title}&page=${page}`;
+    ? `https://www.omdbapi.com?apikey=${APIKEY}c&i=${id}&plot=full`
+    : `https://www.omdbapi.com?apikey=${APIKEY}c&s=${title}&page=${page}`;
   const res = await fetch(url);
   const json = await res.json();
   response.status(200).json(json);
