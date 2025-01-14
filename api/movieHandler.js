35b7e0a2a66e462d6633611a9cb45435bc618788
denev <denev723@gmail.com>
2025-01-14 21:44:35 +0900

    보안 강화

--- api/movieHandler.js
+++ api/movieHandler.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 import fetch from "node-fetch";
 
-const APIKEY = process.env.OMDB_API_KEY;
+const { OMDB_API_KEY } = process.env;
 
 export default async function hanlder(request, response) {
   const { title, page, id } = JSON.parse(request.body);
   const url = id
-    ? `https://www.omdbapi.com?apikey=${APIKEY}c&i=${id}&plot=full`
-    : `https://www.omdbapi.com?apikey=${APIKEY}c&s=${title}&page=${page}`;
+    ? `https://www.omdbapi.com?apikey=${OMDB_API_KEY}c&i=${id}&plot=full`
+    : `https://www.omdbapi.com?apikey=${OMDB_API_KEY}c&s=${title}&page=${page}`;
   const res = await fetch(url);
   const json = await res.json();
   response.status(200).json(json);
